c2f980a6331420b097c157a83659e2b07084d0ce
Murphy Facey (Dev) <developmentmurphy@gmail.com>
2025-05-01 12:48:08 -0500

    chore: add tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
+  <div
+    class="min-h-screen bg-black flex items-center justify-center text-3xl md:text-5xl text-white font-bold"
+  >
     <p>Welcome to V3LT</p>
   </div>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import Vue from "@vitejs/plugin-vue";
+import Tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [Vue()],
+  plugins: [Vue(), Tailwindcss()],
 });
